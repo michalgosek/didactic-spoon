bf41bc50687037faa33649bde38062cc17d90c28
Michal Gosek <inbox@michalgosek.com>
2021-09-17 08:09:30 +0200

    added json response formatting, helper send func.

--- src/api/handlers.ts
+++ src/api/handlers.ts
@@ -8,13 +8,9 @@ export const GetAllUsersHandler = (service: IUserLister) => {
     return async (req: Request, res: Response) => {
         try {
             const data = await service.GetAllUsers();
-            return res.status(200).json({
-                users: data
-            });
+            SendJSONResponse(res, data, 200);
         } catch (err) {
-            return res.status(500).json({
-                message: 'InternalServerError'
-            });
+            SendJSONResponse(res, InternalServerErrorJSON, 500);
         }
     };
 };
@@ -27,45 +23,65 @@ export const GetAllKeySkill = (service: IKeySkillLister) => {
     return async (req: Request, res: Response) => {
         try {
             const data = await service.GetAllKeySkills();
-            return res.status(200).json({
-                skills: data
-            });
+            SendJSONResponse(res, data, 200);
         } catch (err) {
-            return res.status(500).json({
-                message: 'InternalServerError'
-            });
+            SendJSONResponse(res, InternalServerErrorJSON, 500);
         }
     };
 };
 
 interface IUserCountryLister {
-    GetUsersByState(state : String): Promise<IUser[]>
+    GetUsersByState(state: String): Promise<IUser[]>
 }
 
 export const GetUsersByState = (service: IUserCountryLister) => {
     return async (req: Request, res: Response) => {
         try {
-            const state: String = req.body.name; 
+            const state: string = req.body.name;
             if (state.length == 0) {
-                return res.status(500).json({
-                    Error: 'Provide state name query value was undefined',
-                    ExampleCURL: `curl localhost:6060/v1/users/states -d '{"name":"New Hampshire"}' -H "Content-Type: application/json"`
-                });
+                const url: string = `curl localhost:6060/v1/users/states -d '{"name":"New Hampshire"}' -H "Content-Type: application/json"`;
+                const msg: string = "Provide name in the rquest body"
+                const hint: Object = CreateUserResponseHintJSON(msg, url)
+                SendJSONResponse(res, hint, 500);
             }
             const data = await service.GetUsersByState(state);
-            return res.status(200).json({
-                users: data
-            });
+            SendJSONResponse(res, data, 200);
         } catch (err) {
-            return res.status(500).json({
-                message: 'InternalServerError'
-            });
+            console.log("GetUsersByState Op failed.")
+            console.log("Reason:", err)
+            SendJSONResponse(res, InternalServerErrorJSON, 500);
         }
     };
 };
 
+
+function CreateUserResponseHintJSON(msg: string, hint: string): Object {
+    return {
+        Message: msg,
+        Hint: hint,
+    }
+}
+
+const InternalServerErrorJSON: Object = {
+    message: 'InternalServerError'
+}
+
+function SendJSONResponse(res: Response, data: any, code: number) {
+    try {
+        res.header("Content-Type", 'application/json');
+        res.status(code);
+        res.send(JSON.stringify(data, null, 4));
+    } catch (err) {
+        console.log("Response parsing error:", err)
+        res.header("Content-Type", 'application/json');
+        res.status(code);
+        res.send(JSON.stringify(InternalServerErrorJSON, null, 4));
+    }
+}
+
+
 export default {
     GetAllUsersHandler,
     GetAllKeySkill,
-    GetUsersByState, 
+    GetUsersByState,
 }
